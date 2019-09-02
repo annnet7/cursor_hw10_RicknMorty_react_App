4fa16d34e5b965480bb7c0e4d5b20881a73e1e87
annnet7 <51447220+annnet7@users.noreply.github.com>
2019-09-02 13:40:51 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CharacterList from './characters/characters.controller'
+
+
+function App() {
+    return ( 
+        <CharacterList/>
+    );
+}
+
+export default App;
